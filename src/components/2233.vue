831b76a3d103232b4a14b8d5a786042103f1b64b
xiaozw <1410254794@qq.com>
2020-04-16 11:23:07 +0800

    新增文件

---
+++ src/components/2233.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <el-button @click="handleClick">clickMe</el-button>
+    </div>
+</template>
+
+<script>
+    import api from '@/assets/http/api'
+    export default {
+        props: {
+            msg: String
+        },
+        data(){
+            return {
+                testData:'',
+            }
+        },
+        methods:{
+            handleClick:function () {
+                console.log("click me1");
+                api.test.test_http().then(res=>{
+                    console.log(res,'_service');
+                })
+                api.test.test_http1().then(res=>{
+                    console.log(res,'get');
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
